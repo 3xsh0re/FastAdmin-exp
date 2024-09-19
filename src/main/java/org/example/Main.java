907930b5aee9a08e42331fb8a81ee4aa961350be
3xsh0re <1752095049@qq.com>
2024-09-19 23:06:15 +0800

    命令行支持

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, SQLException {
         CmdArgs.getArgs(args);
-        FuzzPoc fuzzPoc = new FuzzPoc("https://test.jonlv.com/");
+        FuzzPoc fuzzPoc = new FuzzPoc(CmdArgs.targetURL);
         fuzzPoc.Exp();
     }
 
