86615ea414cc75a575f3eb781852477a24de62a6
3xsh0re <1752095049@qq.com>
2024-09-26 12:49:29 +0800

    修复参数为空错误

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
         ArgsUtils.getArgs(args);
         if (ArgsUtils.targetURL.isEmpty()&&ArgsUtils.fileName.isEmpty()){
             System.out.println("\033[31;1m[-]" + "请输入正确的参数!" + "\033[0m");
+            return;
         }
-
         if (!ArgsUtils.ifRead) {
             Poc poc = new Poc(ArgsUtils.targetURL);
             poc.Exp();
